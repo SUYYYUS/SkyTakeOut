90cb3282659b19c45fdafd0d7608d6aa348fd131
SUYYYUS <2893796696@qq.com>
2024-12-06 22:04:07 +0800

    feat: '完成用户统计功能‘

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -39,4 +40,20 @@ public class ReportController {
         TurnoverReportVO turnoverStatistic = reportService.getTurnoverStatistic(begin, end);
         return Result.success(turnoverStatistic);
     }
+
+    /**
+     * 用户统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/userStatistics")
+    @ApiOperation("获取用户数据据")
+    public Result<UserReportVO> userStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+                                                  @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("用户数据统计：{}，{}", begin, end);
+        UserReportVO userStatistics = reportService.getUserStatistics(begin, end);
+        return Result.success(userStatistics);
+    }
+
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,10 +2,13 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import jodd.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +26,8 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 统计指定时间区域的每天收入
@@ -44,7 +49,6 @@ public class ReportServiceImpl implements ReportService {
         turnoverReportVO.setDateList(join);
 
         List<Double> res = new ArrayList<>();
-
         //再计算营业额
         for (LocalDate time : dateTimeList) {
             //营业额状态是订单列表中状态为已完成的订单
@@ -66,5 +70,54 @@ public class ReportServiceImpl implements ReportService {
         String sumList = StringUtil.join(res, ",");
         turnoverReportVO.setTurnoverList(sumList);
         return turnoverReportVO;
+    }
+
+    /**
+     * 统计用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+
+        //先计算日期
+        List<LocalDate> dateTimeList = new ArrayList<>(); //用于存放日期范围内的每天日期
+        dateTimeList.add(begin);
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateTimeList.add(begin);
+        }
+        String dateList = StringUtil.join(dateTimeList, ",");//拼接成字符串
+
+        //统计用户总量
+        List<Integer> userTotalList = new ArrayList<>();
+        //统计新增用户数量
+        List<Integer> newUserList = new ArrayList<>();
+
+        for (LocalDate time : dateTimeList) {
+            //当天的开始时间和结束时间
+            LocalDateTime beginTime = LocalDateTime.of(time, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(time, LocalTime.MAX);
+            Map map = new HashMap();
+            //查询总用户数量
+            map.put("begin", beginTime);
+            int userCount = userMapper.getUserCount(map);
+            userTotalList.add(userCount);
+            //查询新增用户
+            map.put("end", endTime);
+            int newUserCount = userMapper.getUserCount(map);
+            newUserList.add(newUserCount);
+        }
+        //转换为字符串
+        String userCountList = StringUtils.join(userTotalList, ",");
+        String newUserCountList = StringUtils.join(newUserList, ",");
+        //封装对象
+        UserReportVO userReportVO = new UserReportVO();
+        userReportVO.setDateList(dateList);
+        userReportVO.setTotalUserList(userCountList);
+        userReportVO.setNewUserList(newUserCountList);
+        //返回结果
+        return userReportVO;
     }
 }
