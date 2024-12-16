e5b501cb94dc73f4dd8db628d0ba100b76ca248a
SUYYYUS <2893796696@qq.com>
2024-12-16 17:22:27 +0800

    refactor: '少提交一个'

--- sky-server/src/main/java/com/sky/service/BloomFilterService.java
+++ sky-server/src/main/java/com/sky/service/BloomFilterService.java
@@ -14,29 +14,29 @@ public class BloomFilterService {
     /**
      * 私有的不可变的bloomFilter对象
      */
-    @Resource
-    private BloomFilterUtil bloomFilterUtil;
+/*    @Resource
+    private BloomFilterUtil bloomFilterUtil;*/
 
     /**
      * 向过滤器添加元素
      * @param element
      */
-    public void add(String element){
+/*    public void add(String element){
         bloomFilterUtil.bloomFilter.put(element);
-    }
+    }*/
 
     /**
      * 检查过滤器中是否存在该元素
      * @param element
      * @return
      */
-    public String check(String element){
+/*    public String check(String element){
         if(bloomFilterUtil.bloomFilter.mightContain(element)){
             return element + "可能存在于集合中";
         }else {
             return element + "不可能存在集合中";
         }
-    }
+    }*/
 
 
 }
