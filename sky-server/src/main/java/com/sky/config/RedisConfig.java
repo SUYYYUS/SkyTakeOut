20975271a46a7b97ef67597fa72bb4bc57dbf209
SUYYYUS <2893796696@qq.com>
2024-12-16 16:42:36 +0800

    feat: '指定Redisson操作的数据库'

--- sky-server/src/main/java/com/sky/config/RedisConfig.java
+++ sky-server/src/main/java/com/sky/config/RedisConfig.java
@@ -13,26 +13,8 @@ public class RedisConfig {
         //配置类
         Config config = new Config();
         //添加redis地址，这里添加的是单节点，还没有做集群
-        config.useSingleServer().setAddress("redis://192.168.223.141:6379").setPassword("123456");
+        config.useSingleServer().setAddress("redis://192.168.223.141:6379").setPassword("123456").setDatabase(2);
         return Redisson.create(config);
     }
-
-/*    @Bean
-    public RedissonClient redissonClient2(){
-        //配置类
-        Config config = new Config();
-        //添加redis地址，这里添加的是单节点，还没有做集群
-        config.useSingleServer().setAddress("redis://192.168.223.140:6380");
-        return Redisson.create(config);
-    }
-
-    @Bean
-    public RedissonClient redissonClient3(){
-        //配置类
-        Config config = new Config();
-        //添加redis地址，这里添加的是单节点，还没有做集群
-        config.useSingleServer().setAddress("redis://192.168.223.140:6381");
-        return Redisson.create(config);
-    }*/
 
 }
